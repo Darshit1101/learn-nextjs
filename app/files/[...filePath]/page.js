718e62f88702db16d0bb63913b42295dc302ce1f
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-07-04 14:26:51 +0530

    update

---
+++ app/files/[...filePath]/page.js
@@ -0,0 +1,11 @@
+export default async function Page({ params }) {
+  const { filePath } = await params;
+  console.log('File Path:', filePath);
+
+  return (
+    <div>
+      <h1>File Content for====== {filePath}</h1>
+      <h4>Filke join========== /{filePath.join('/')}</h4>
+    </div>
+  );
+}
